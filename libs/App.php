ce0c17e08c1bb3f3361914f34adf6c60a020d52a
Harald Lapp <harald@octris.org>
2016-01-22 01:16:04 +0100

    allow to specify a default action for command
    defaults to the 'help' command

--- libs/App.php
+++ libs/App.php
@@ -33,24 +33,33 @@ class App extends \Aaparser\Args
     /**
      * Import and add a command from a php script.
      *
-     * @param   string      $name           Name of command to import.
-     * @param   string      $class          Name of class to import command from, must implent App\ICommand.
-     * @return  \Aaparser\Command           Instance of new object.
+     * @param   string      $name               Name of command to import.
+     * @param   string      $class              Name of class to import command from, must implent App\ICommand.
+     * @param   callable    $default_command    Optional default command (default: help).
+     * @return  \Aaparser\Command               Instance of new object.
      */
-    public function importCommand($name, $class, array $settings = array())
+    public function importCommand($name, $class, array $settings = array(), callable $default_command = null)
     {
         if (!is_subclass_of($class, '\Octris\Cli\App\ICommand')) {
             throw new \InvalidArgumentException('Class is not a valid command.');
         }
-        
+
         $cmd = $this->addCommand($name);
         $cmd->setAction(function(array $options, array $operands) use ($class) {
             $instance = new $class();
             $instance->run($options, $operands);
         });
-        
+
+        if (is_null($default_command)) {
+            $default_command = function() use ($cmd) {
+                $this->printHelp($cmd);
+            };
+        }
+
+        $cmd->setDefaultAction($default_command);
+
         $class::configure($cmd);
-        
+
         return $cmd;
     }
 
